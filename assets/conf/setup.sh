e397234d44864ef8b9ba3798b39fada5377a1238
ikizema <kizema.ivan@gmail.com>
2015-12-17 13:50:37 -0500

    mgt custom data

--- assets/conf/setup.sh
+++ assets/conf/setup.sh
@@ -33,6 +33,7 @@ ln -s /opt/liferay/jboss* /opt/liferay/jboss &&
 # Configure Liferay
 mkdir /opt/liferay/deploy && cp /assets/packages/license* /opt/liferay/deploy/ &&
 cp -R /assets/conf/liferay/portal-ext.properties /opt/liferay/ &&
+cp -R /assets/conf/liferay/data/* opt/liferay/data/ &&
 cp -R /assets/conf/liferay/jboss/* /opt/liferay/jboss/ &&
 
 # Exit
